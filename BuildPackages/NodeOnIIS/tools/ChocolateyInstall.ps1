c19e2025bd16b0af93be720026e579c018357d9e
fschwiet <fschwiet@gmail.com>
2013-05-15 15:22:44 -0700

    adding IIS dependencies to node package, removing node.js for now

--- BuildPackages/NodeOnIIS/tools/ChocolateyInstall.ps1
+++ BuildPackages/NodeOnIIS/tools/ChocolateyInstall.ps1
@@ -1,7 +1,13 @@
 
 try {
 
-    Install-ChocolateyPackage 'NodeJS 0.10.5' 'msi' '/passive /norestart' 'http://nodejs.org/dist/v0.10.5/node-v0.10.5-x86.msi' 'http://nodejs.org/dist/v0.10.5/x64/node-v0.10.5-x64.msi'
+    cinst IIS-WebServerRole -source windowsfeatures
+    cinst IIS-ManagementScriptingTools -source windowsfeatures
+    cinst IIS-WindowsAuthentication -source windowsfeatures
+    cinst IIS-ManagementConsole -source windowsfeatures
+    cinst IIS-ASPNET -source windowsfeatures
+    cinst IIS-ASPNET45 -source windowsfeatures
+
     Install-ChocolateyPackage 'IIS Node' 'msi' '/passive /norestart' 'http://go.microsoft.com/?linkid=9784330' 'http://go.microsoft.com/?linkid=9784331'
     cinstm UrlRewrite
 

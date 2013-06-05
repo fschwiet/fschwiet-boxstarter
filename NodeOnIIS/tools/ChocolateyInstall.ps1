7ee6bfc76bf218b74391a8d9c508b910a8b83719
fschwiet <fschwiet@gmail.com>
2013-06-05 01:30:46 -0700

    install Carbon with NodeOnIIS

--- NodeOnIIS/tools/ChocolateyInstall.ps1
+++ NodeOnIIS/tools/ChocolateyInstall.ps1
@@ -15,6 +15,9 @@ try {
 
     Install-ChocolateyPackage 'IIS Node' 'msi' '/passive /norestart' 'http://go.microsoft.com/?linkid=9784330' 'http://go.microsoft.com/?linkid=9784331'
 
+    # http://get-carbon.org/
+    Install-ChocolateyZipPackage "Carbon" "http://bitbucket.org/splatteredbits/carbon/downloads/Carbon-1.1.0.zip"  "$env:USERPROFILE\Documents\WindowsPowerShell\Modules\Carbon"
+
     Write-ChocolateySuccess 'NodeOnIIS'
 } catch {
   Write-ChocolateyFailure 'NodeOnIIS' $($_.Exception.Message)
